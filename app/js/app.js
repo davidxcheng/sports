fc47fadba04e5608590b861a1255382b037e68f1
leo <leo@solutionblvd.com>
2023-10-10 00:02:15 +0200

    Fixade uppgiften galant

--- app/js/app.js
+++ app/js/app.js
@@ -13,27 +13,78 @@ window.addEventListener("load", async () => {
 // Returnera en array sorterad på poäng (fallande)
 // Varje item i arrayen motsvarar en rad i serietabellen
 function calculateStandings(gamesPlayed) {
-  // Skapa en variabel för serietabellen
+  var teams = new Array(16);
 
-  // Loopa över gamesPlayed
-    // För varje match, räkna ut poäng för vardera lag..
-    // ..och uppdatera serietabellen
-
-  // Sortera serietabellen efter poäng (vi bryr oss inte om målskillnad)
-
-  return [
-    {
-      teamName: "AIK",
-      matchesPlayed: 2,
-      won: 1,
-      draws: 0,
-      lost: 1,
-      points: 3
+  for (const match of gamesPlayed) {
+    if (match.matchday == 1) {
+      teams[match.home.team.id] = {
+        teamName: match.home.team.name,
+        matchesPlayed: 0,
+        won: 0,
+        draws: 0,
+        lost: 0,
+        points: 0
+        }
+      teams[match.away.team.id] = {
+        teamName: match.away.team.name,
+        matchesPlayed: 0,
+        won: 0,
+        draws: 0,
+        lost: 0,
+        points: 0
+        }
     }
-  ];
+  }
+
+  for (const match of gamesPlayed) {
+    var result = "unknown";
+    teams[match.home.team.id].matchesPlayed += 1
+    teams[match.away.team.id].matchesPlayed += 1
+    if (match.home.goals > match.away.goals) {
+      result = '1';
+      teams[match.home.team.id].points += 3
+      teams[match.home.team.id].won += 1
+      teams[match.away.team.id].lost += 1
+    } 
+    if (match.home.goals < match.away.goals) {
+      result = '2';
+      teams[match.away.team.id].points += 3
+      teams[match.home.team.id].lost += 1
+      teams[match.away.team.id].won += 1
+    }
+    if (match.home.goals == match.away.goals) {
+      result = 'X';
+      teams[match.home.team.id].points += 1
+      teams[match.away.team.id].points += 1
+      teams[match.home.team.id].draws += 1
+      teams[match.away.team.id].draws += 1
+    }
+  }
+
+return teams;
 }
+
+function render(standings) {
+  var elOutput = document.getElementById("output");
+  var markup = "";
+
+  // Sortera standings efter poäng i fallande ordning
+  standings.sort((a, b) => b.points - a.points);
+
+  for (const row of standings) {
+    console.log(row.teamName);
+    markup += `  <tr>
+      <td>${row.teamName}</td>
+      <td>${row.matchesPlayed}</td>
+      <td>${row.won}</td>
+      <td>${row.draws}</td>
+      <td>${row.lost}</td>
+      <td>${row.points}</td>
+    </tr>`;
+  }
+  
+  elOutput.innerHTML = markup;
+}
+
 
 // Tar emot en sorterad array som representerar serietabellen
-function render(standings) {
-  console.log("standings", standings)
-}
